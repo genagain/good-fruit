90ac4d4e2b4e79dad6e313553333772b7c6d26c4
Gen Ohta <myself@genohta.com>
2021-02-23 16:21:08 -0800

    fresh

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <div className="font-logo">
-        Good Fruits
-      </div>
-      <div>Hey hey hey</div>
-      <input type="text" />
-    </div>
+    <div>Hello world!</div>
   );
 }
 
